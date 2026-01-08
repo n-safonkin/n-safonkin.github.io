f8e4a436561a5975025c8b23b9d36c5db8fa18fd
n-safonkin <safonkin.nik@gmail.com>
2026-01-08 17:35:20 +0100

    Create papers.js

---
+++ assets/papers.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest("[data-toggle]");
+  if (!btn) return;
+  const id = btn.getAttribute("data-toggle");
+  const box = document.getElementById(id);
+  if (!box) return;
+  box.classList.toggle("is-hidden");
+});
